1f50ff61d644e1b99c31b1801ed10583ee4ced5c
victor blær <victor.blaer@gmail.com>
2026-02-23 23:07:14 +0000

    chore: configure vite and gh-actions for new epstein-network-financial-forensic-dashboard repository

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/epstein-forensic-finance/',
+  base: '/epstein-network-financial-forensic-dashboard/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
